51c2d0c0b690f1b91d5d28e9107250ab97eb8577
stevelyall <stevenlyall@gmail.com>
2024-02-10 21:29:59 -0800

    Add Leaflet map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import TTMap from './components/TTMap';
 
+
 function App() {
+  
+  const [userLocation, setUserLocation] = useState(undefined);
+  const [isLoading, setisLoading] = useState(true);
+
+  // load location when app loads
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+  
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log('got position', position)
+      setUserLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      })
+      setisLoading(false);
+    });
+  };
+  
+  const header = (<header className="App-header"></header>);
+  const loadingMsg = (<p>Loading...</p>);
+  // TODO nicer loading message
+  // TODO handle permissions
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-        <TTMap />
+      {header}
+      {isLoading && (loadingMsg)} 
+      {userLocation && (<TTMap mapLocation={userLocation}/>)}
     </div>
   );
 }
--- src/components/TTMap.js
+++ src/components/TTMap.js
@@ -8,7 +8,7 @@ import { useMap } from "react-leaflet/hooks";
 
 // TODO Typescript? npm install -D @types/leaflet
 
-export default function TTMap() {
+export default function TTMap({mapLocation}) {
   const mapStyles = {
     width: "100vw",
     height: "90vh",
@@ -18,7 +18,7 @@ export default function TTMap() {
     <React.Fragment>
       <MapContainer
         style={mapStyles}
-        center={[51.505, -0.09]}
+        center={{lat: mapLocation.lat, lng: mapLocation.lng}} //[location.lat, location.long]
         zoom={13}
         scrollWheelZoom={true}
       >
