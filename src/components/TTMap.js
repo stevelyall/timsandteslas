437a0f37a6f6c4310194edc1d16220ca00aee21e
stevelyall <stevenlyall@gmail.com>
2024-02-10 18:10:29 -0800

    add Leaflet map

--- src/components/TTMap.js
+++ src/components/TTMap.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React from "react";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { Marker } from "react-leaflet/Marker";
+import { Popup } from "react-leaflet/Popup";
 
-export default function TTMap(){
+import { useMap } from "react-leaflet/hooks";
 
+// TODO Typescript? npm install -D @types/leaflet
 
-    return (
-      <React.Fragment>
-      <p>apiKey {process.env.REACT_APP_GOOGLE_MAPS_API_KEY}</p>
+export default function TTMap() {
+  const mapStyles = {
+    width: "100vw",
+    height: "90vh",
+  };
 
-      
+  return (
+    <React.Fragment>
+      <MapContainer
+        style={mapStyles}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      {/* </div> */}
     </React.Fragment>
-
-    )
+  );
 }
