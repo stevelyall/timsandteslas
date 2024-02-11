216e558dee89db0a51a537f7db21713e655fb5b8
stevelyall <stevenlyall@gmail.com>
2024-02-10 17:45:20 -0800

    remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import TTMap from './components/TTMap';
 
@@ -6,12 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <TTMap />
       </header>
+        <TTMap />
     </div>
   );
 }
--- src/components/TTMap.js
+++ src/components/TTMap.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 export default function TTMap(){
+
+
     return (
+      <React.Fragment>
       <p>apiKey {process.env.REACT_APP_GOOGLE_MAPS_API_KEY}</p>
+
+      
+    </React.Fragment>
+
     )
 }
